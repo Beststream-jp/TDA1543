fe308019f647dbadd24de330659454e03f9ca3d5
ryood <kawanabe@gmail.com>
2016-01-18 15:29:30 +0900

    BYTES_PER_BURSTを1に

--- PSoC/I2S_Test/PrototypingKit_DMA.cydsn/main.c
+++ PSoC/I2S_Test/PrototypingKit_DMA.cydsn/main.c
@@ -14,7 +14,7 @@
 #define TABLE_LENGTH 256
 
 /* Defines for DMA_1 */
-#define DMA_1_BYTES_PER_BURST 2
+#define DMA_1_BYTES_PER_BURST 1
 #define DMA_1_REQUEST_PER_BURST 1
 #define DMA_1_SRC_BASE (CYDEV_SRAM_BASE)
 #define DMA_1_DST_BASE (CYDEV_PERIPH_BASE)
@@ -24,17 +24,43 @@
 uint8 DMA_1_Chan;
 uint8 DMA_1_TD[1];
 
-uint16 sawTable[TABLE_LENGTH];
+uint8 sawTable[TABLE_LENGTH];
+uint8 sineTable[TABLE_LENGTH];
+
+const uint8 sineTable8[TABLE_LENGTH/2] = 
+{
+	128, 134, 140, 147, 153, 159, 165, 171,
+	177, 183, 188, 194, 199, 204, 209, 214,
+	218, 223, 227, 231, 234, 238, 241, 244,
+	246, 248, 250, 252, 253, 254, 255, 255,
+	255, 255, 255, 254, 253, 252, 250, 248,
+	246, 244, 241, 238, 234, 231, 227, 223,
+	218, 214, 209, 204, 199, 194, 188, 183, 
+	177, 171, 165, 159, 153, 147, 140, 134, 
+	128, 122, 115, 109, 103,  97,  91,  85,
+	 79,  73,  68,  62,  57,  52,  47,  42,
+	 37,  33,  29,  25,  22,  18,  15,  12,
+	 10,   7,   6,   4,   2,   1,   1,   0,
+	  0,   0,   1,   1,   2,   4,   6,   7,
+	 10,  12,  15,  18,  22,  25,  29,  33,
+	 37,  42,  47,  52,  57,  62,  68,  73,
+	 79,  85,  91,  97, 103, 109, 115, 122
+};	 
 
 int main()
 {
-    int i, v;
+    int i;
     
-    // sawTableの生成
+    // 16bit sawTableの生成
     for (i = 0; i < TABLE_LENGTH; i += 2) {
-        v = i * (0x10000 / TABLE_LENGTH);
-        sawTable[i]   = v;
-        sawTable[i+1] = v;
+        sawTable[i]   = i * (0x100 / TABLE_LENGTH);
+        sawTable[i+1] = 0;
+    }
+    
+    // 16bit sineTableの生成
+    for (i = 0; i < TABLE_LENGTH / 2; i++) {
+        sineTable[i * 2]     = sineTable8[i];
+        sineTable[i * 2 + 1] = 0;
     }
     
     CyGlobalIntEnable; /* Enable global interrupts. */
@@ -48,7 +74,7 @@ int main()
         HI16(DMA_1_SRC_BASE), HI16(DMA_1_DST_BASE));
     DMA_1_TD[0] = CyDmaTdAllocate();
     CyDmaTdSetConfiguration(DMA_1_TD[0], TABLE_LENGTH, DMA_1_TD[0], TD_INC_SRC_ADR);
-    CyDmaTdSetAddress(DMA_1_TD[0], LO16((uint32)sawTable), LO16((uint32)I2S_1_TX_CH0_F0_PTR));
+    CyDmaTdSetAddress(DMA_1_TD[0], LO16((uint32)sineTable), LO16((uint32)I2S_1_TX_CH0_F0_PTR));
     CyDmaChSetInitialTd(DMA_1_Chan, DMA_1_TD[0]);
     CyDmaChEnable(DMA_1_Chan, 1);
 
