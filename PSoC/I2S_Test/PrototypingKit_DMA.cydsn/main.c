b4faeb1ea13f3947305a5937e62952b3aee223fb
ryood <kawanabe@gmail.com>
2016-01-18 18:47:56 +0900

    Sin波出力

--- PSoC/I2S_Test/PrototypingKit_DMA.cydsn/main.c
+++ PSoC/I2S_Test/PrototypingKit_DMA.cydsn/main.c
@@ -11,7 +11,7 @@
 */
 #include <project.h>
 
-#define TABLE_LENGTH 256
+#define TABLE_LENGTH 128
 
 /* Defines for DMA_1 */
 #define DMA_1_BYTES_PER_BURST 1
@@ -24,9 +24,10 @@
 uint8 DMA_1_Chan;
 uint8 DMA_1_TD[1];
 
-int8 sineTable[TABLE_LENGTH];
 
-const uint8 sineTable8[TABLE_LENGTH/2] = 
+int8 sineTable[TABLE_LENGTH*2];
+
+const uint8 sineTable8[TABLE_LENGTH] = 
 {
 	128, 134, 140, 147, 153, 159, 165, 171,
 	177, 183, 188, 194, 199, 204, 209, 214,
@@ -50,10 +51,10 @@ int main()
 {
     int i;
 
-    // 16bit sineTableの生成
-    for (i = 0; i < TABLE_LENGTH / 2; i++) {
-        sineTable[i * 2]     = (int)sineTable8[i] - 128;
-        sineTable[i * 2 + 1] = 0;
+    // 符号付き16bit sineTableの生成
+    for (i = 0; i < TABLE_LENGTH; i++) {
+        sineTable[i*2]   = (int)sineTable8[i] - 128;
+        sineTable[i*2+1] = 0;
     }
     
     CyGlobalIntEnable; /* Enable global interrupts. */
@@ -66,7 +67,7 @@ int main()
     DMA_1_Chan = DMA_1_DmaInitialize(DMA_1_BYTES_PER_BURST, DMA_1_REQUEST_PER_BURST, 
         HI16(DMA_1_SRC_BASE), HI16(DMA_1_DST_BASE));
     DMA_1_TD[0] = CyDmaTdAllocate();
-    CyDmaTdSetConfiguration(DMA_1_TD[0], TABLE_LENGTH, DMA_1_TD[0], TD_INC_SRC_ADR);
+    CyDmaTdSetConfiguration(DMA_1_TD[0], (TABLE_LENGTH*2), DMA_1_TD[0], TD_INC_SRC_ADR);
     CyDmaTdSetAddress(DMA_1_TD[0], LO16((uint32)sineTable), LO16((uint32)I2S_1_TX_CH0_F0_PTR));
     CyDmaChSetInitialTd(DMA_1_Chan, DMA_1_TD[0]);
     CyDmaChEnable(DMA_1_Chan, 1);
