2e1acac3f77885498fa358b1e9b20fc2d48958fa
ryood <kawanabe@gmail.com>
2016-01-22 10:58:37 +0900

    PrototypingKit_DDS：　DDSで波形出力

--- PSoC/I2S_Test/PrototypingKit_DDS.cydsn/main.c
+++ PSoC/I2S_Test/PrototypingKit_DDS.cydsn/main.c
@@ -7,10 +7,14 @@
  * CONFIDENTIAL AND PROPRIETARY INFORMATION
  * WHICH IS THE PROPERTY OF your company.
  *
+ * 2016.01.22 正弦波を出力
+ *
  * ========================================
 */
 #include <project.h>
 #include "wavetable.h"
+
+#define SAMPLE_CLOCK    187500u
 
 #define TABLE_SIZE      1024
 #define BUFFER_SIZE     32     
@@ -35,44 +39,68 @@ uint8 DMA_0_TD[1];
 uint8 DMA_1_Chan;
 uint8 DMA_1_TD[1];
 
-volatile uint8 waveBuffer[BUFFER_SIZE];
-volatile int16 waveIndex = 0;
+volatile uint8 waveBuffer_0[BUFFER_SIZE];
+volatile uint8 waveBuffer_1[BUFFER_SIZE];
 
-inline void byteSwap2(uint8* p)
+volatile uint32 tuningWord_0;
+volatile uint32 tuningWord_1;
+volatile uint32 phaseRegister_0 = 0;
+volatile uint32 phaseRegister_1 = 0;
+
+void setDDSParameter_0(uint16 frequency)
 {
-    uint8 tmp;
-    tmp = p[1];
-    p[1] = p[0];
-    p[0] = tmp;
+    tuningWord_0 = (((uint64)frequency << 32) / SAMPLE_CLOCK);
 }
 
-void generateWave()
+void setDDSParameter_1(uint16 frequency)
 {
-    int i;
+    tuningWord_1 = (((uint64)frequency << 32) / SAMPLE_CLOCK);
+}
+
+void generateWave_0()
+{
+    int i, index;
     uint8* p8;
     
     // 波形をバッファに転送
     for (i = 0; i < BUFFER_SIZE; i+=2) {
-        p8 = (uint8 *)(sineTable + waveIndex);
-        waveBuffer[i]   = *(p8 + 1);
-        waveBuffer[i+1] = *p8;
+        phaseRegister_0 += tuningWord_0;
+        index = phaseRegister_0 >> 22;
         
-        waveIndex++;
-        if (waveIndex == TABLE_SIZE) {
-            waveIndex = 0;
-        }
+        p8 = (uint8 *)(sineTable + index);
+        waveBuffer_0[i]   = *(p8 + 1);
+        waveBuffer_0[i+1] = *p8;
+    }
+}
+
+void generateWave_1()
+{
+    int i, index;
+    uint8* p8;
+    
+    // 波形をバッファに転送
+    for (i = 0; i < BUFFER_SIZE; i+=2) {
+        phaseRegister_1 += tuningWord_1;
+        index = phaseRegister_1 >> 22;
+        
+        p8 = (uint8 *)(sineTable + index);
+        waveBuffer_1[i]   = *(p8 + 1);
+        waveBuffer_1[i+1] = *p8;
     }
 }
 
 CY_ISR (dma_0_done_handler)
 {
     Pin_Check_0_Write(1u);
-    generateWave();
+    generateWave_0();
     Pin_Check_0_Write(0u);
 }
 
 CY_ISR (dma_1_done_handler)
 {
+    Pin_Check_1_Write(1u);
+    generateWave_1();
+    Pin_Check_1_Write(0u);
 }
 
 CY_ISR (i2s_1_tx_handler)
@@ -84,7 +112,10 @@ CY_ISR (i2s_1_tx_handler)
 
 int main()
 {
-    generateWave();
+    setDDSParameter_0(1000);
+    setDDSParameter_1(10000);
+    generateWave_0();
+    generateWave_1();
     
     CyGlobalIntEnable; /* Enable global interrupts. */
 
@@ -95,7 +126,7 @@ int main()
         HI16(DMA_0_SRC_BASE), HI16(DMA_0_DST_BASE));
     DMA_0_TD[0] = CyDmaTdAllocate();
     CyDmaTdSetConfiguration(DMA_0_TD[0], BUFFER_SIZE, DMA_0_TD[0], DMA_0__TD_TERMOUT_EN | TD_INC_SRC_ADR);
-    CyDmaTdSetAddress(DMA_0_TD[0], LO16((uint32)waveBuffer), LO16((uint32)I2S_1_TX_CH0_F0_PTR));
+    CyDmaTdSetAddress(DMA_0_TD[0], LO16((uint32)waveBuffer_0), LO16((uint32)I2S_1_TX_CH0_F0_PTR));
     CyDmaChSetInitialTd(DMA_0_Chan, DMA_0_TD[0]);
     CyDmaChEnable(DMA_0_Chan, 1);
 
@@ -104,7 +135,7 @@ int main()
         HI16(DMA_1_SRC_BASE), HI16(DMA_1_DST_BASE));
     DMA_1_TD[0] = CyDmaTdAllocate();
     CyDmaTdSetConfiguration(DMA_1_TD[0], BUFFER_SIZE, DMA_1_TD[0], DMA_1__TD_TERMOUT_EN | TD_INC_SRC_ADR);
-    CyDmaTdSetAddress(DMA_1_TD[0], LO16((uint32)waveBuffer), LO16((uint32)I2S_1_TX_CH0_F1_PTR));
+    CyDmaTdSetAddress(DMA_1_TD[0], LO16((uint32)waveBuffer_1), LO16((uint32)I2S_1_TX_CH0_F1_PTR));
     CyDmaChSetInitialTd(DMA_1_Chan, DMA_1_TD[0]);
     CyDmaChEnable(DMA_1_Chan, 1);
 
