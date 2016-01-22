705c7b3a726bcbc93bcd48581a97d94d43cc0c9e
ryood <kawanabe@gmail.com>
2016-01-22 16:30:11 +0900

    Add: Project Ext XTal

---
+++ PSoC/I2S_Test/PrototypingKit_Ext_Xtal.cydsn/main.c
@@ -0,0 +1,30 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include <project.h>
+
+int main()
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+
+    for(;;)
+    {
+        /* Place your application code here. */
+        Pin_LED1_Write(1u);
+        CyDelay(500);
+        Pin_LED1_Write(0u);
+        CyDelay(500);
+    }
+}
+
+/* [] END OF FILE */
