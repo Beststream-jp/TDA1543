b3e5b2b0a1901ee21107211fa962903ed211965a
ryood <kawanabe@gmail.com>
2016-01-24 19:09:43 +0900

    PrototypingKit_DDS: L/Rのバッファを同一のものに変更

--- PSoC/I2S_Test/PrototypingKit_DDS.cydsn/main.c
+++ PSoC/I2S_Test/PrototypingKit_DDS.cydsn/main.c
@@ -40,23 +40,24 @@ uint8 DMA_1_Chan;
 uint8 DMA_1_TD[1];
 
 volatile uint8 waveBuffer_0[BUFFER_SIZE];
-volatile uint8 waveBuffer_1[BUFFER_SIZE];
+//volatile uint8 waveBuffer_1[BUFFER_SIZE];
 
 volatile uint32 tuningWord_0;
-volatile uint32 tuningWord_1;
+//volatile uint32 tuningWord_1;
 volatile uint32 phaseRegister_0 = 0;
-volatile uint32 phaseRegister_1 = 0;
+//volatile uint32 phaseRegister_1 = 0;
 
 void setDDSParameter_0(uint16 frequency)
 {
     tuningWord_0 = (((uint64)frequency << 32) / SAMPLE_CLOCK);
 }
 
+/*
 void setDDSParameter_1(uint16 frequency)
 {
     tuningWord_1 = (((uint64)frequency << 32) / SAMPLE_CLOCK);
 }
-
+*/
 void generateWave_0()
 {
     int i, index;
@@ -71,8 +72,9 @@ void generateWave_0()
         waveBuffer_0[i]   = *(p8 + 1);
         waveBuffer_0[i+1] = *p8;
     }
+    
 }
-
+/*
 void generateWave_1()
 {
     int i, index;
@@ -88,7 +90,7 @@ void generateWave_1()
         waveBuffer_1[i+1] = *p8;
     }
 }
-
+*/
 CY_ISR (dma_0_done_handler)
 {
     Pin_Check_0_Write(1u);
@@ -96,12 +98,14 @@ CY_ISR (dma_0_done_handler)
     Pin_Check_0_Write(0u);
 }
 
+/*
 CY_ISR (dma_1_done_handler)
 {
     Pin_Check_1_Write(1u);
     generateWave_1();
     Pin_Check_1_Write(0u);
 }
+*/
 
 CY_ISR (i2s_1_tx_handler)
 {
@@ -112,10 +116,10 @@ CY_ISR (i2s_1_tx_handler)
 
 int main()
 {
-    setDDSParameter_0(1000);
-    setDDSParameter_1(10000);
+    setDDSParameter_0(10000);
+    //setDDSParameter_1(1000);
     generateWave_0();
-    generateWave_1();
+    //generateWave_1();
     
     CyGlobalIntEnable; /* Enable global interrupts. */
 
@@ -135,12 +139,12 @@ int main()
         HI16(DMA_1_SRC_BASE), HI16(DMA_1_DST_BASE));
     DMA_1_TD[0] = CyDmaTdAllocate();
     CyDmaTdSetConfiguration(DMA_1_TD[0], BUFFER_SIZE, DMA_1_TD[0], DMA_1__TD_TERMOUT_EN | TD_INC_SRC_ADR);
-    CyDmaTdSetAddress(DMA_1_TD[0], LO16((uint32)waveBuffer_1), LO16((uint32)I2S_1_TX_CH0_F1_PTR));
+    CyDmaTdSetAddress(DMA_1_TD[0], LO16((uint32)waveBuffer_0), LO16((uint32)I2S_1_TX_CH0_F1_PTR));
     CyDmaChSetInitialTd(DMA_1_Chan, DMA_1_TD[0]);
     CyDmaChEnable(DMA_1_Chan, 1);
 
     ISR_DMA_0_Done_StartEx(dma_0_done_handler);
-    ISR_DMA_1_Done_StartEx(dma_1_done_handler);
+    //ISR_DMA_1_Done_StartEx(dma_1_done_handler);
     ISR_I2S_1_TX_StartEx(i2s_1_tx_handler);
     
     while(0u != (I2S_1_ReadTxStatus() & (I2S_1_TX_FIFO_0_NOT_FULL | I2S_1_TX_FIFO_1_NOT_FULL)))
